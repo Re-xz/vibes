9f08d291ac6bdbeb84d1bfd2a003ad3a3c7e66ce
Re-xz only <124796779+Re-xz@users.noreply.github.com>
2023-10-19 04:12:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ load_dotenv()
 
 api_id = int(os.getenv("API_ID", "29486311"))
 api_hash = os.getenv("API_HASH", "ffdc688dc4eee8d2585cb24155188432")
-bot_token = os.getenv("BOT_TOKEN", "6652895969:AAF640FWhEeF4BjKLtId6IGYTNIOIr6XVG0")
+bot_token = os.getenv("BOT_TOKEN", "6738154204:AAEjVRKFsCH1WzmHH6Ig7RB9TJgTw0W5_RQ")
 # =========================================================== #
 
 db_url = os.getenv("DB_URL", "mongodb+srv://fadhil:fadhil123@cluster0.jvnx5r6.mongodb.net/?retryWrites=true&w=majority")
 db_name = os.getenv("DB_NAME", "fadhil") #bisa diganti sesuai kebutuhan
 # =========================================================== #
 
-channel_1 = int(os.getenv("CHANNEL_1", "-1001976588675"))
-channel_2 = int(os.getenv("CHANNEL_2", "-1001610283560")) #untuk group comentar user
-channel_log = int(os.getenv("CHANNEL_LOG", "-1001710331413"))
+channel_1 = int(os.getenv("CHANNEL_1", "-1002118815380"))
+channel_2 = int(os.getenv("CHANNEL_2", "-1001861463708")) #untuk group comentar user
+channel_log = int(os.getenv("CHANNEL_LOG", "-1002118815380"))
 # =========================================================== #
 
-id_admin = int(os.getenv("ID_ADMIN", "957521020"))
+id_admin = int(os.getenv("ID_ADMIN", "5800485350"))
 # =========================================================== #
 
-batas_kirim = int(os.getenv("BATAS_KIRIM", "2"))
+batas_kirim = int(os.getenv("BATAS_KIRIM", "5"))
 # =========================================================== #
 
 biaya_kirim = int(os.getenv("BIAYA_KIRIM", "20"))
