678b201e1d8d5f0d0dd47b7172f83b34fea2027a
Re-xz only <124796779+Re-xz@users.noreply.github.com>
2023-10-19 04:07:24 +0700

    Update helper.py

--- plugins/helper/helper.py
+++ plugins/helper/helper.py
@@ -55,8 +55,8 @@ class Helper():
         link_1 = await self.bot.export_chat_invite_link(config.channel_1)
         link_2 = await self.bot.export_chat_invite_link(config.channel_2)
         markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton('CHANNEL CPF', url=link_1), InlineKeyboardButton('GROUP CPF', url=link_2)],
-            [InlineKeyboardButton('COBA LAGI', url=f'https://t.me/{self.bot.username}?start=start')]
+            [InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ ᴄᴘғ', url=link_1), InlineKeyboardButton('ɢʀᴏᴜᴘ ᴄᴘғ', url=link_2)],
+            [InlineKeyboardButton('ᴄᴏʙᴀ ʟᴀɢɪ', url=f'https://t.me/{self.bot.username}?start=start')]
         ])
         await self.bot.send_message(self.user_id, config.pesan_join, reply_to_message_id=self.message.id, reply_markup=markup)
 
