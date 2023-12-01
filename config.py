d9aba1582c3699373c12af3ab47b7b8e9dc1ff1b
Re-xz only <124796779+Re-xz@users.noreply.github.com>
2023-12-01 09:57:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,53 +9,49 @@ bot_token = os.getenv("BOT_TOKEN", "")
 # =========================================================== #
 
 db_url = os.getenv("DB_URL", "mongodb+srv://kikoy:kikoy6969@cluster0.vooxu.mongodb.net/?retryWrites=true&w=majority")
-db_name = os.getenv("DB_NAME", "ctpfess") #bisa diganti sesuai kebutuhan
+db_name = os.getenv("DB_NAME", "fwbx") #bisa diganti sesuai kebutuhan
 # =========================================================== #
 
-channel_1 = int(os.getenv("CHANNEL_1", "-1001197188587"))
-channel_2 = int(os.getenv("CHANNEL_2", "-1001680642097")) #untuk group comentar user
-channel_log = int(os.getenv("CHANNEL_LOG", "-1002037994205"))
+channel_1 = int(os.getenv("CHANNEL_1", "-1001801771890"))
+channel_2 = int(os.getenv("CHANNEL_2", "-1001913052601")) #untuk group comentar user
+channel_log = int(os.getenv("CHANNEL_LOG", "-1002109996381"))
 # =========================================================== #
 
 id_admin = int(os.getenv("ID_ADMIN", "957521020"))
 # =========================================================== #
 
-batas_kirim = int(os.getenv("BATAS_KIRIM", "5"))
+batas_kirim = int(os.getenv("BATAS_KIRIM", "2"))
 # =========================================================== #
 
 biaya_kirim = int(os.getenv("BIAYA_KIRIM", "20"))
 # =========================================================== #
 
-hastag = os.getenv("HASTAG", "#ss #aa #ctpboy #ctpask #ctpgirl #ctpspill #ctpstory").replace(" ", "|").lower()
+hastag = os.getenv("HASTAG", "#fwbgirl #fwbboy #fwbspill #fwbstory").replace(" ", "|").lower()
 # =========================================================== #
 
-pic_boy = os.getenv("PIC_BOY", "https://telegra.ph/file/c67bd36023648dc777bd9.jpg")
-pic_girl = os.getenv("PIC_GIRL", "https://telegra.ph/file/cb885bcbf5081dbd45f27.jpg")
+pic_boy = os.getenv("PIC_BOY", "https://telegra.ph/boy-12-01-28")
+pic_girl = os.getenv("PIC_GIRL", "https://telegra.ph/ss-girl-12-01")
 # =========================================================== #
 
 pesan_join = os.getenv("PESAN_JOIN", """
-Halo👋 kamu harus bergabung di CHANNEL & GROUP Terlebih dahulu untuk mengirim pesan ke @ctpmenfess
-
-Join Ke Channel & Group Kemudian Tekan /help ⤵️
+Kamu harus join di CHANNEL & GROUP yang ada dibawah untuk dapat mengirim pesan ke @friendwithbenefitx
 """)
 start_msg = os.getenv("START_MSG", """
 Halo {mention} 😍
 
-ctp bot adalah bot autopost, semua pesan yang dikirimkan ke bot ini akan otomatis dikirim ke channel @ctpmenfess, gunakan hastag dibawah untuk mengirim pesan:
+fwbbasexbot adalah bot promote yang dapat digunakan untuk mencari teman, pacar, dll serta dapat digunakan untuk mengirim menfess, gunakan hastag dibawah untuk mengirim pesan:
 
-#CtpBoy / #CtpGirl : untuk mencari teman, pasangan, partner dll.
-#CtpAsk : untuk bertanya.
-#CtpSpill : untuk spill sesuatu.
-#CtpStory : untuk berbagi cerita/pengalaman.
+#FwbBoy / #FwbGirl : untuk mencari teman, pasangan, partner dll.
+#FwbSpill : untuk spill sesuatu.
+#FwbStory : untuk berbagi cerita/pengalaman.
 """)
 
 gagalkirim_msg = os.getenv("GAGAL_KIRIM", """
 {mention} pesanmu gagal terkirim 🙅, harap gunakan hastag : 
 
-#CtpBoy / #CtpGirl : untuk mencari teman, pasangan, partner dll.
-#CtpAsk : untuk bertanya
-#CtpSpill : untuk spill sesuatu
-#CtpStory : untuk berbagi cerita/pengalaman.
+#FwbBoy / #FwbGirl : untuk mencari teman, pasangan, partner dll.
+#FwbSpill : untuk spill sesuatu
+#FwbStory : untuk berbagi cerita/pengalaman.
 
-untuk pertanyaan silahkan gabung @CariTemanPacar_Online
+jangan lupa join @caripartnerfwb
 """)
