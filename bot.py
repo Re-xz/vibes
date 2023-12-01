1a9f1549158eaf7d892abb715dada1f639395be7
Re-xz only <124796779+Re-xz@users.noreply.github.com>
2023-12-01 10:04:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ class Bot(Client):
         self.id_bot = bot_me.id
         data.append(self.id_bot)
         await self.set_bot_commands([
-            BotCommand('status', '📊 check status' 'help', '❓ cek panduan')
+            BotCommand('status', '📊 check status'), BotCommand('help', '❓ tutorial kirim'
         ], BotCommandScopeAllPrivateChats())
         
         print('BOT TELAH AKTIF')
